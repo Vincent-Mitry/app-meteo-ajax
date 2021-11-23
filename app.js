56aac726d180fee3b11c78e8dffc9f7033e1beed
Vincent Mitry <vinz.mitry@gmail.com>
2021-11-23 21:45:51 +0100

    Ajout du ContentEditable et modification de la fonction main en fonction du parametre IP

--- app.js
+++ app.js
@@ -16,15 +16,22 @@ function capitalize(str){
     return str[0].toUpperCase() + str.slice(1);
 }
 // Async / Await => permet de rendre une promesse synchrone
-async function main(){
-    // 1. Choper l'adresse IP du PC qui ouvre la page : https://api.ipify.org?format=json
-    const ip = await fetch('https://api.ipify.org?format=json')
-        .then(resultat => resultat.json())
-        .then(json => json.ip)
-    // 2. Choper la ville grâce à l'adresse IP : http://ip-api.com/json/adresseDuMec
-    const ville = await fetch('http://ip-api.com/json/' + ip)
-        .then(resultat => resultat.json())
-        .then(json => json.city)
+// On ajoute le paramètre par défaut 'withIP'
+async function main(withIP = true){
+    let ville;
+
+    if(withIP){
+        // 1. Choper l'adresse IP du PC qui ouvre la page : https://api.ipify.org?format=json
+        const ip = await fetch('https://api.ipify.org?format=json')
+            .then(resultat => resultat.json())
+            .then(json => json.ip)
+        // 2. Choper la ville grâce à l'adresse IP : http://ip-api.com/json/adresseDuMec
+        ville = await fetch('http://ip-api.com/json/' + ip)
+            .then(resultat => resultat.json())
+            .then(json => json.city)
+    } else {
+        ville = document.querySelector('#ville').textContent;
+    }
     // 3. Choper les infos météo grâce à la ville : http://api.openweathermap.org/data/2.5/weather?q=VilleDuMec&appid=c0d4a23f973c3521df89aa75a2c37ac1&lang=fr&units=metric
     const meteo = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=c0d4a23f973c3521df89aa75a2c37ac1&lang=fr&units=metric')
         .then(resultat => resultat.json())
@@ -53,6 +60,21 @@ function displayWeatherInfos(data){
 
     document.body.className = conditions.toLowerCase();
 }
+
+let ville = document.querySelector('#ville');
+
+// Lors du 'click' sur l'élément '#ville', son contenu devient modifiable
+ville.addEventListener('click', () => {
+    ville.contentEditable = true;
+})
+// Lorsqu'on appuie sur la touche 'entrée' (e.keycode === 13), on stoppe l'éxécution de de l'événement (on ne vas plus à la ligne) et on arrête d'éditer le contenu de '#ville'. On appelle ensuite la fonction 'main'
+ville.addEventListener('keydown', (e) => {
+    if(e.keyCode === 13){
+        e.preventDefault();
+        ville.contentEditable = false;
+        main(false);
+    }
+})
 
 main();
 
